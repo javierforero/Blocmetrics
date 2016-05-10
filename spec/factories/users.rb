44164b66d849f386b16e89b2584022997306cce0
Javier Forero <javierforero01@gmail.com>
2016-05-10 08:25:48 -0500

    completer user sign in story

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,10 @@
 FactoryGirl.define do
+  pw = Faker::Internet.password(8)
   factory :user do
-    
+    name Faker::Name.name
+    email Faker::Internet.email
+    password pw
+    password_confirmation pw
+    confirmed_at Faker::Date.between(1.year.ago, Date.today)
   end
 end
