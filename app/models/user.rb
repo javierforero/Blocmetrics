b2dad83a33a0305c68571e7283e0c85f64e566f1
Javier Forero <javierforero01@gmail.com>
2016-05-12 14:15:29 -0500

    corrected rspec errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,4 @@ class User < ActiveRecord::Base
   # before_save { self.email = email.downcase }
 
   validates :name, length: {minimum: 1, maximum: 100}, presence: true
-  validates :email, presence: true, uniqueness:{case_sensitive: false}, length:{minimum: 3, maximum: 254}
 end
