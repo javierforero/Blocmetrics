7e48843733378c427ac82079e564309e7b1b561d
Javier Forero <javierforero01@gmail.com>
2016-05-16 08:10:36 -0500

    merged

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
-  # before_save { self.email = email.downcase }
-
+         
   validates :name, length: {minimum: 1, maximum: 100}, presence: true
+  has_many :registered_apps, dependent: :destroy
 end
