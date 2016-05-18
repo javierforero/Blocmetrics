ad5f6a2c9e40328d8aca1efceaa60e99d15b33b9
Javier Forero <javierforero01@gmail.com>
2016-05-18 14:39:23 -0500

    wrote api. It is still not working but it will

---
+++ app/controllers/api/events_controller.rb
@@ -0,0 +1,40 @@
+class API::EventsController < ApplicationController
+
+  skip_before_action  :verify_authenticity_token
+  before_filter :set_access_control_headers
+
+  def create
+
+    registered_app = RegisteredApp.find_by(url: request.ENV['HTTP_ORIGIN'])
+    @event = Event.new(event_params)
+
+    if registered_app == nil
+
+      render json: "Sorry. This is an unregistered application", status: :unprocessable_entity
+    elsif @event.valid?
+      @event.save!
+      render json: @event, status: :created
+    else
+      render json: {errors: @event.errors}, status: :unprocessable_entity
+    end
+  end
+
+  def preflight
+    head 200
+  end
+
+  def set_access_control_headers
+
+    headers['Access-Control-Allow-Origin'] = '*'
+
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+
+    headers['Access-Control-Allow-Headers'] = 'Content-Type'
+  end
+  
+  private
+
+  def event_params
+    params.require(:event).permit(:name)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,9 @@ Rails.application.routes.draw do
     resources :registered_apps
   end
 
+  namespace :api, defaults: { format: :json } do
+    match '/events', to: 'events#preflight', via: [:options]
+    resources :events, only: [:create]
+  end
+
 end
