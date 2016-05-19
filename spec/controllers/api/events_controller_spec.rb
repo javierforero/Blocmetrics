a4389bd4a68a6130ce8a3cf6c52e1d4e857fb4f3
Javier Forero <javierforero01@gmail.com>
2016-05-18 21:58:43 -0500

    got api and rspec workinggit add . '

--- spec/controllers/api/events_controller_spec.rb
+++ spec/controllers/api/events_controller_spec.rb
@@ -7,11 +7,16 @@ RSpec.describe API::EventsController, type: :controller do
 
   context "URL corresponds to a registered app" do
     before do
+      @request.env['HTTP_ORIGIN'] = "hi.com"
       post :create, event: {name: "testing rspec"}
     end
     describe "create an event" do
       it "POST create successfully creates a new event" do
         expect(response).to have_http_status(:success)
+      end
+
+      it "Assures event name is set correctly" do
+        expect(my_app.events.find_by(name:"testing rspec")).not_to be_nil
       end
     end
   end
