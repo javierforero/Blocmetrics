07cc20fbe22b585178e32c90aba38491254cb97c
Javier Forero <javierforero01@gmail.com>
2016-06-11 15:37:46 -0500

    fixed dependencies bug

--- app/models/registered_app.rb
+++ app/models/registered_app.rb
@@ -1,6 +1,6 @@
 class RegisteredApp < ActiveRecord::Base
   belongs_to :user
-  has_many :events
+  has_many :events, dependent: :destroy
   validates :name, presence: true
   validates :url, presence: true
 end
